08f04e72d6faed7b3f095dba9d578c895719968a
Rhys <DouglasRM@cardiff.ac.uk>
2021-02-19 22:33:55 +0000

    [MVP-0] Add shell of tomorrows work.

--- RD.CanMusicMakeYouRunFaster/RD.CanMusicMakeYouRunFaster.FakeResponseServer/Models/PlayHistoryItem.cs
+++ RD.CanMusicMakeYouRunFaster/RD.CanMusicMakeYouRunFaster.FakeResponseServer/Models/PlayHistoryItem.cs
@@ -1,6 +1,29 @@
 ﻿namespace RD.CanMusicMakeYouRunFaster.FakeResponseServer.Models
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Class used to represent a PlayHistoryItem from the spotify API.
+    /// This contains a subset of items used by the real PlayHistoryItem, as 
+    /// not all properties are supported by EntityFrameworkCore.
+    /// </summary>
     public class PlayHistoryItem
     {
+        /// <summary>
+        /// DateTime of when the track was played.
+        /// </summary>
+        public DateTime? PlayedAt { get; set; }
+
+        /// <summary>
+        /// SimpleTrack object, holding track information.
+        /// </summary>
+        [Key]
+        public SimpleTrack Track { get; set; } = default!;
+
+        /// <summary>
+        /// Context object, holding context information.
+        /// </summary>
+        public Context Context { get; set; } = default!;
     }
 }
