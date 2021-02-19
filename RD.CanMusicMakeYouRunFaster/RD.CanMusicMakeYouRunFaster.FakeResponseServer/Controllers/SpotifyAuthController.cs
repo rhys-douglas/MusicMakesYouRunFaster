16197c6dccc3ba976afa7af30cc07b6529391c29
Rhys <DouglasRM@cardiff.ac.uk>
2021-02-19 14:01:13 +0000

    [MVP-0] Mid add OAuth Token Acquisition.

--- RD.CanMusicMakeYouRunFaster/RD.CanMusicMakeYouRunFaster.FakeResponseServer/Controllers/SpotifyAuthController.cs
+++ RD.CanMusicMakeYouRunFaster/RD.CanMusicMakeYouRunFaster.FakeResponseServer/Controllers/SpotifyAuthController.cs
@@ -3,10 +3,14 @@
     using Microsoft.AspNetCore.Mvc;
     using DbContext;
     using System.Threading.Tasks;
+    using SpotifyAPI.Web;
+    using System.Collections.Generic;
+    using System;
 
     /// <summary>
     /// Fake spotify controller, which act as spotify's auth service.
     /// </summary>
+    [ApiController]
     public class SpotifyAuthController : ControllerBase
     {
         private DataRetrievalContext context;
@@ -24,9 +28,24 @@
         /// Gets a PKCE Auth Token
         /// </summary>
         /// <returns> A valid PKCE Auth Token </returns>
-        public async Task<JsonResult> GetPKCEAuthToken()
+        public async Task<PKCETokenResponse> GetPKCEAuthToken(PKCETokenRequest request)
         {
+            if (request == null)
+            {
+                throw new System.Exception("Token request is null.");
+            }
 
+            var TokenResponse = new PKCETokenResponse
+            {
+                AccessToken = "",
+                CreatedAt = DateTime.Now,
+                ExpiresIn = 3600,
+                RefreshToken = "other random string",
+                Scope = scopes,
+                TokenType = "Bearer"
+            };
+
+            return TokenResponse;
         }
     }
 }
