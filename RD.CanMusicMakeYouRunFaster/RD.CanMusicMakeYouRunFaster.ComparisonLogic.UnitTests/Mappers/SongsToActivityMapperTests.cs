63100a99baab31395a544b6becab9826a845940e
Rhys <DouglasRM@cardiff.ac.uk>
2021-03-04 18:27:43 +0000

    [MVP-5] Now know what I'm doing.

---
+++ RD.CanMusicMakeYouRunFaster/RD.CanMusicMakeYouRunFaster.ComparisonLogic.UnitTests/Mappers/SongsToActivityMapperTests.cs
@@ -0,0 +1,16 @@
+﻿namespace RD.CanMusicMakeYouRunFaster.ComparisonLogic.UnitTests.Mappers
+{
+    using NUnit.Framework;
+    using RD.CanMusicMakeYouRunFaster.ComparisonLogic.Mappers;
+
+    public class SongsToActivityMapperTests
+    {
+        private SongsToActivityMapper sut;
+
+        [Test]
+        public void MapWithCorrectParams_CorrectResultsReturned()
+        {
+            sut = new SongsToActivityMapper();
+        }
+    }
+}
---
+++ RD.CanMusicMakeYouRunFaster/RD.CanMusicMakeYouRunFaster.ComparisonLogic/Mappers/SongsToActivityMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace RD.CanMusicMakeYouRunFaster.ComparisonLogic.Mappers
+{
+    /// <summary>
+    /// Maps songs to an activity.
+    /// </summary>
+    public class SongsToActivityMapper
+    {
+    }
+}
