1b9214e4c94b136dafb7dbdea1363fc68601a6e8
Rhys <DouglasRM@cardiff.ac.uk>
2021-03-16 17:14:13 +0000

    [EDF-0] Begin to add first UI screen.

--- RD.CanMusicMakeYouRunFaster/RD.CanMusicMakeYouRunFaster.AndroidApp/MainActivity.cs
+++ RD.CanMusicMakeYouRunFaster/RD.CanMusicMakeYouRunFaster.AndroidApp/MainActivity.cs
@@ -14,7 +14,7 @@
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.welcome);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
