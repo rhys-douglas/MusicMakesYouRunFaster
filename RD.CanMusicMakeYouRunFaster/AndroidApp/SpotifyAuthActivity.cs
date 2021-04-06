bd6b470708754102e9b7707c8ebd0fc6faf3f588
Rhys <DouglasRM@cardiff.ac.uk>
2021-04-06 17:46:44 +0100

    [EDF-0] Need to change spotify redirect URI.

--- RD.CanMusicMakeYouRunFaster/AndroidApp/SpotifyAuthActivity.cs
+++ RD.CanMusicMakeYouRunFaster/AndroidApp/SpotifyAuthActivity.cs
@@ -4,13 +4,10 @@
     using Android.Content;
     using Android.Content.PM;
     using Android.OS;
-    using Android.Runtime;
-    using Android.Views;
     using Android.Widget;
+    using SpotifyAPI.Web.Auth;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
+    using Xamarin.Auth;
 
     [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
     [IntentFilter(new[] { Android.Content.Intent.ActionView },
@@ -18,6 +15,8 @@
     DataScheme = "myapp")]
     public class SpotifyAuthActivity : Activity
     {
+        private static readonly EmbedIOAuthServer StravaAuthServer = new EmbedIOAuthServer(new Uri("http://localhost:5000/spotifytoken"), 5000);
+
         Button spotifyLoginButton = null;
         TextView infoText = null;
 
@@ -36,7 +35,71 @@
 
         private async void SpotifyButton_Click(object sender, EventArgs e)
         {
+            await StravaAuthServer.Start();
+            var auth = new OAuth2Authenticator(
+                "1580ff80db9a43e589eee411deba30b0",
+                "a325e33f157345ca90d9477b5a7f2f7e",
+                "user-read-private,user-read-recently-played",
+                new Uri("https://accounts.spotify.com/authorize"),
+                new Uri("http://localhost:5000/spotifyToken"),
+                new Uri("https://accounts.spotify.com/api/token"));
+            auth.Completed += SpotifyAuth_Completed;
+            var ui = auth.GetUI(this);
+            StartActivity(ui);
+        }
 
+        private async void SpotifyAuth_Completed(object sender, AuthenticatorCompletedEventArgs e)
+        {
+            if (e.IsAuthenticated)
+            {
+                await StravaAuthServer.Stop();
+                var request = new OAuth2Request(
+                    "GET",
+                    new Uri("https://api.spotify.com/v1/me/player/recently-played"),
+                    null,
+                    e.Account);
+
+                var stravaResponse = await request.GetResponseAsync();
+                var json = stravaResponse.GetResponseText();
+                infoText.Text += json;
+            }
         }
     }
+
+    /*
+     var authToken = string.Empty;
+
+            var (verifier, challenge) = PKCEUtil.GenerateCodes();
+            await SpotifyAuthServer.Start();
+
+            // Temporary auth server lsitens for Spotify callback.
+            SpotifyAuthServer.AuthorizationCodeReceived += async (sender, response) =>
+            {
+                await SpotifyAuthServer.Stop();
+                PKCETokenResponse token = await new OAuthClient().RequestToken(
+                  new PKCETokenRequest(SpotifyClientId, response.Code, SpotifyAuthServer.BaseUri, verifier));
+                authToken = JsonConvert.SerializeObject(token);
+            };
+
+            // Make spotify auth call.
+            var request = new LoginRequest(SpotifyAuthServer.BaseUri, SpotifyClientId, LoginRequest.ResponseType.Code)
+            {
+                CodeChallenge = challenge,
+                CodeChallengeMethod = "S256",
+                Scope = new List<string> { UserReadPrivate, UserReadRecentlyPlayed }
+            };
+
+            Uri uri = request.ToUri();
+            try
+            {
+                BrowserUtil.Open(uri);
+                Task.Delay(10000).Wait();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to open URL, manually open: {0}", uri);
+            }
+
+            return new JsonResult(authToken);
+    */
 }
